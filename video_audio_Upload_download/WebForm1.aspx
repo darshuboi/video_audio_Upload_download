b4a42799e12fe069f050021b9b4557aae180ed7c
Darshan Raval <darahan95582@gmail.com>
2024-04-02 21:25:14 +0530

    Add project files.

---
+++ video_audio_Upload_download/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="video_audio_Upload_download.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+              <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" Width="226px">
+                <Columns>
+                    <asp:TemplateField HeaderText="id">
+                        <ItemTemplate>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="name">
+                        <ItemTemplate>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+
+
+
+
+              <br />
+              <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+
+
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ video_audio_Upload_download/WebForm1.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web.UI.WebControls;
+
+namespace video_audio_Upload_download
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string s = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\video_audio_Upload_download\video_audio_Upload_download\video_audio_Upload_download\App_Data\file_db.mdf;Integrated Security=True;";
+
+        SqlConnection con;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetDataForFileId();
+            }
+        }
+
+        public void getcon()
+        {
+            con = new SqlConnection(s);
+            con.Open();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fileName = Path.GetFileName(FileUpload1.FileName);
+                fileName = fileName.Replace(" ", "");
+                FileUpload1.SaveAs(Server.MapPath("~/Upload/") + fileName);
+                string filePath = "Upload/" + fileName;
+
+                string videoTag = "<video width='450' controls><source src='" + filePath + "' type='video/mp4'></video>";
+                Literal1.Text = videoTag;
+
+                getcon();
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO file_tbl (name, contenttype) VALUES (@name, @path)", con);
+                cmd.Parameters.AddWithValue("@name", fileName);
+                cmd.Parameters.AddWithValue("@path", filePath);
+                cmd.ExecuteNonQuery();
+                GetDataForFileId(); 
+
+                con.Close(); 
+            }
+        }
+
+        public void GetDataForFileId()
+        {
+            getcon();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM file_tbl", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+            con.Close();
+        }
+    }
+}
